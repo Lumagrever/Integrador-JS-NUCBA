e61975942304e1f38a162a28eb7130c06f3be363
Lumagrever <lumagrever@hotmail.com>
2023-04-19 11:28:51 -0300

    Se agregan funciones en main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 //LOCAL STORAGE
-
+//Seteamos el carrito
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const saveLocalStorage = (productsList) =>
@@ -26,7 +26,7 @@ const renderCardsProducts = (product) => {
             <p class="product-name">${nombre}</p>
             <span class="price-product">$${precio}</span>
             <small> ${sector} ${tipo}</small>
-            <button
+            <button class="btn-add"
             data-id=${id}
             data-name=${nombre}
             data-bid=${precio}
@@ -167,27 +167,237 @@ const closeOnScroll = () => {
         return;
     }
     overlay.classList.remove('show-overlay');
+    barsMenu.classList.remove('open-menu');
+    cartMenu.classList.remove('open-cart');
 };
 
 
 const closeOnClick = (e) => {
     if (!e.target.classList.contains('navbar-link')) return;
-    barsMenu.classList.remove('open-menu');
     overlay.classList.remove('show-overlay');
+    barsMenu.classList.remove('open-menu');
+    cartMenu.classList.remove('open-cart');
 }
 
 
 const closeOnOverlayClick = () => {
+    overlay.classList.remove('show-overlay');
     barsMenu.classList.remove('open-menu');
     cartMenu.classList.remove('open-cart');
-    overlay.classList.remove('show-overlay');
 }
+
+// CARRITO
+
+const productsCart = document.querySelector(".cart-container");
+
+const total = document.querySelector(".total");
+
+const buyBtn = document.querySelector(".btn-buy");
+
+const cartBubble = document.querySelector(".cart-bubble");
+
+const successModal = document.querySelector(".add-modal");
+
+const deleteBtn = document.querySelector(".btn-delete");
+
+const renderCardProduct = (cartProduct) => {
+    const {id, nombre, precio, img, quantity} = cartProduct
+    return `
+    <div class="cart-item">
+    <img src="${img}" alt="${nombre}">
+    <div class="item-info">
+        <h3 class="item-title">${nombre}</h3>
+        <p class="item-bid">Precio actual</p>
+        <span class="item-price">$${precio}</span>
+    </div>
+    <div class="item-handler">
+        <span class="quantity-handler down" data-id="${id}">-</span>
+        <span class="item-quantity">${quantity}</span>
+        <span class="quantity-handler up" data-id="${id}">+</span>
+    </div>
+</div>
+    `
+};
+
+const renderCart = () => {
+    if (!cart.length) {
+        productsCart.innerHTML = `<p class="empty-msg">No hay productos en el carrito</p>`;
+        return;
+    }
+    productsCart.innerHTML = cart.map(renderCardProduct).join("");
+};
+
+// Funcion para precio total
+
+const getCartTotal = () => 
+    cart.reduce((acc,cur) => acc + Number(cur.precio) * cur.quantity, 0);
+
+// Funcion para renderizar el precio total de la compra con sus sumas correspondientes
+
+const showTotal = () => {
+    total.innerHTML = `$${getCartTotal().toFixed(2)}`
+};
+
+// Funcion para renderizar la cantidad de productos en la burbujita del carrito
+
+const renderCartBubble = () => {
+    cartBubble.textContent = cart.reduce((acc, cur) => acc + cur.quantity, 0);
+};
+
+// Deshabilitar el boton de compra o habilitarlo
+
+const disableBtn = (btn) => {
+    if(!cart.length) {
+        btn.classList.add('disabled');
+    } else {
+        btn.classList.remove('disabled');
+    }
+};
+
+// Funcion para analizar el estado del carrito
+
+const checkCartState = () => {
+    saveLocalStorage(cart);
+    renderCart();
+    showTotal();
+    disableBtn(buyBtn);
+    disableBtn(deleteBtn);
+    renderCartBubble();
+}
+
+// Funcion para agregar un producto al carrito
+
+const addProduct = e => {
+    if(!e.target.classList.contains('btn-add')) return;
+    const { id, name, precio, img } = e.target.dataset;
+
+    const product = productData(id, name, precio, img);
+
+    if(isExistingCartProduct(product)){
+        addUnitToProduct(product);
+        showSuccessModal('Se agregó una unidad del producto al carrito')
+    } else {
+        createCartProduct(product);
+        showSuccessModal('El producto se ha agregado al carrito')
+    }
+    checkCartState();
+};
+
+// Funcion para mostrar el modal
+
+const showSuccessModal = msg => {
+    successModal.classList.add('active-modal');
+    successModal.textContent = msg;
+    setTimeout (() => {
+        successModal.classList.remove('active-modal');
+    }, 1500);
+};
+
+// Funcion para crear un producto en el carrito
+
+const createCartProduct = product => {
+    cart = [... cart, {... product, quantity: 1 }];
+};
+
+// Funcion para agregar una unidad de producto
+
+const addUnitToProduct = product => {
+    cart = cart.map(cartProduct =>
+        cartProduct.id == product.id
+        ? {... cartProduct, quantity: cartProduct.quantity + 1}
+        : cartProduct
+        );
+};
+
+// Funcion para verificar si existe un producto en el carrito
+
+const isExistingCartProduct = product => {
+    return cart.find(item => item.id == product.id)
+};
+
+// Funcion que va a crear un objeto para la data del producto
+
+const productData = (id, nombre, precio, img) => {
+    return {id, nombre, precio, img};
+};
+
+// Funcion para manipular el boton "menos"
+
+const handleMinusBtnEvent = id => {
+    const existingCartProduct = cart.find(item => item.id == id);
+
+    if(existingCartProduct.quantity == 1) {
+        if(window.confirm('¿Desea eliminar el producto del carrito?')){
+            //Borrar el producto
+            removeProductFromCart(existingCartProduct);
+        }
+        return
+    }
+    substractProductUnit(existingCartProduct)
+};
+
+
+//Funcion para restar la unidad del producto
+
+const substractProductUnit = existingCartProduct => {
+    cart = cart.map (product => {
+        return product.id == existingCartProduct.id
+        ? { ... product, quantity: Number(product.quantity) -1}
+        : product;
+    });
+};
+
+// Funcion para remover el producto del carrito
+
+const removeProductFromCart = existingCartProduct => {
+    cart = cart.filter(product => product.id !== existingCartProduct.id);
+    checkCartState();
+};
+
+// Funcion para cuando se presione el "+"
+
+const handlePlusBtnEvent = id => {
+    const existingCartProduct = cart.find(item => item.id == id);
+    addUnitToProduct(existingCartProduct);
+};
+
+const handleQuantity = e => {
+    if(e.target.classList.contains('down')) {
+        handleMinusBtnEvent(e.target.dataset.id);
+    } else if (e.target.classList.contains('up')) {
+        handlePlusBtnEvent(e.target.dataset.id);
+    }
+    checkCartState();
+};
+
+const resetCartItems = () => {
+    cart = [];
+    checkCartState();
+};
+
+// Funcion para realizar una de las acciones del carrito
+const completeCartAction = (confirmMsg, successMsg) => {
+    if (!cart.lenght) return;
+    if (window.confirm(confirmMsg)){
+        resetCartItems();
+        alert(successMsg);
+    }
+};
+
+const completeBuy = () => {
+    completeCartAction('¿Desea completar tu compra?', '¡Gracias por tu compra!');
+};
+
+const deleteCart = () => {
+    completeCartAction('¿Desea borrar todo?', 'No hay productos en el carrito');
+};
+
 
 
 // Init
 
  const init = () => {
-    renderProducts()
+    renderProducts();
     typesProducts.addEventListener('click', applyFilter);
     btnLoad.addEventListener('click', showMoreProducts);
     btnBars.addEventListener('click', toggleMenu);
@@ -195,6 +405,17 @@ const closeOnOverlayClick = () => {
     barsMenu.addEventListener('click', closeOnClick);
     overlay.addEventListener('click', closeOnOverlayClick);
     window.addEventListener('scroll', closeOnScroll);
+    overlay.classList.remove('show-overlay');
+    document.addEventListener('DOMContentLoaded', renderCart);
+    document.addEventListener('DOMContentLoaded', showTotal);
+    products.addEventListener('click', addProduct);
+    productsCart.addEventListener('click', handleQuantity);
+    buyBtn.addEventListener('click', completeBuy);
+    deleteBtn.addEventListener('click', deleteCart);
+    renderCartBubble();
+    disableBtn(buyBtn);
+    disableBtn(deleteBtn);
+    //checkCartState();
 }
 
 init();
